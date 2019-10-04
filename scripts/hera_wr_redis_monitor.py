4b06a5fcc280d4901ea105cd34d4ca65ec7d234c
reeveress <zuhraa470@gmail.com>
2019-10-04 03:32:36 +0000

    Make poll script actually loop

--- scripts/hera_wr_redis_monitor.py
+++ scripts/hera_wr_redis_monitor.py
@@ -10,26 +10,33 @@ def main():
 
     parser = argparse.ArgumentParser(description='VUART shell for WR-LEN')
     parser.add_argument('hosts', metavar='hosts', type=str, nargs='*',
-                    help='WR hosts to poll. Default: heraNode{0..32}Wr')
+                    help='WR hosts to poll. Default: heraNode{0..32}wr')
+    parser.add_argument('-t', dest='polltime', type=float, default=30,
+                    help='Minimum time between polling cycles in seconds. Default: 30s')
     r = redis.Redis('redishost')
 
     args = parser.parse_args()
     
     if len(args.hosts) == 0:
-        hosts = ['heraNode%dWr' % x for x in range(32)]
+        hosts = ['heraNode%dwr' % x for x in range(32)]
     else:
         hosts = args.hosts
 
     print('Begging WR-LEN status polling. Hosts are:')
     print(hosts)
-    for host in hosts:
-        try:
-            ip = socket.gethostbyname(host)
-        except socket.gaierror:
-            continue
-        wr = WrLen(host)
-        stats = wr.process_stats()
-        r.hmset('status:wr:%s' % wr.host, stats)
+    while(True):
+        start_time = time.time()
+        for host in hosts:
+            try:
+                ip = socket.gethostbyname(host)
+            except socket.gaierror:
+                continue
+            wr = WrLen(host)
+            stats = wr.process_stats()
+            r.hmset('status:wr:%s' % wr.host, stats)
+        extra_wait = args.polltime - (time.time() - start_time)
+        if extra_wait > 0:
+            time.sleep(extra_wait)
 
 if __name__ == '__main__':
     main()
