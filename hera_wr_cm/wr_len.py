d7d71b5ea69631d369b874120f9043a3f536a6c1
reeveress <zuhraa470@gmail.com>
2019-10-04 18:00:05 +0000

    Process version string into dictionary

--- hera_wr_cm/wr_len.py
+++ hera_wr_cm/wr_len.py
@@ -13,30 +13,50 @@ class WrLen(VUART_shell):
         except socket.gaierror:
             print("hostname %s not known!" % host)
 
+    def process_ver(self):
+        rv = {}
+        rv['board_info_str'] = self.vuart.sendCommand('ver').decode('iso-8859-1') #OMG, actually using more than ascii!!
+        for line in [x.strip() for x in rv['board_info_str'].split('\n')]:
+            if line.startswith('WR Core build'):
+                rv['wr_build'] = line.split(':')[-1]
+            elif line.startswith('Build on'):
+                rv['wr_build_date'] = line.split(':')[-1]
+            elif line.startswith('WR Core build'):
+                rv['wr_rom_id'] = line.split(':')[-1]
+            elif line.startswith('FRU') or line.startswith('GW'):
+                name, val = line.split(':', 1)
+                name = name.strip().replace(' ', '_').lower()
+                val = val.strip()
+                rv['wr_%s' % name] = val
+        rv['sw_build_date'] = self.ver_date
+        return rv
+
     def process_stats(self):
         rv = {}
         stats = self.vuart.sendCommand('stat').decode().split('\n')
         for line in stats:
             if line.startswith('WR mode'):
-                rv['mode'] = line.split(':')[-1]
+                rv['mode'] = line.split(':')[-1].strip()
             elif line.startswith('wr0'):
-                #rv['wr0'] = {}
                 for var in line.lstrip('wr0 -> ').split(' '):
                     try:
                         key, val = var.split(':')
+                        key = key.strip()
+                        val = val.strip()
                         try:
                             #rv['wr0'][key] = int(val)
-                            rv['wr0:%s' % key] = int(val)
+                            rv['wr0_%s' % key] = int(val)
                         except ValueError:
                             #rv['wr0'][key] = val
-                            rv['wr0:%s' % key] = val
+                            rv['wr0_%s' % key] = val
                     except ValueError:
                         pass
             elif line.startswith('wr1'):
-                #rv['wr1'] = {}
                 for var in line.lstrip('wr1 -> ').split(' '):
                     try:
                         key, val = var.split(':')
+                        key = key.strip()
+                        val = val.strip()
                         try:
                             rv['wr1:%s' % key] = int(val)
                         except ValueError:
@@ -45,8 +65,12 @@ class WrLen(VUART_shell):
                         pass
             elif line.startswith('temp'):
                 rv['temp'] = line.split(' ')[1]
-        rv['board_info'] = self.vuart.sendCommand('ver').decode('iso-8859-1') #OMG, actually using more than ascii!!
-        rv['sw_build_date'] = self.ver_date
+        return rv
+
+    def gather_keys(self):
+        rv = {}
+        rv.update(self.process_ver())
+        rv.update(self.process_stats())
         rv['ip'] = self.ip
         rv['timestamp'] = datetime.datetime.now().isoformat()
         return rv
